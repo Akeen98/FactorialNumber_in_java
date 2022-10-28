82195367ce83d362e2ad4ae8a76ba5ce97978679
Akeen Bnadara <akeenbandara9@gmail.com>
2022-10-28 23:40:26 +0530

    create Cal for find factorial Number

---
+++ FactorialNum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class FactorialNum{  
+ public static void main(String args[]){  
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter Your Number for answering Factoria ");
+  int number = sc.nextInt();
+    int i,fact=1;
+    
+  for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+}  
